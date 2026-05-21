019be29be56dbcdf8bb139af84dcf0ed328f8c93
Chamika Jayan <jayanchamika274@gmail.com>
2026-05-21 22:04:08 +0530

    initail new setup

--- frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-function App() {
-  return (
-    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-amber-300">Smart Tourism</h1>
-    </div>
-  );
-}
-export default App;
